99ef5fdf3d4acd9863913db922653611c090b075
geornti <geonti10@gmail.com>
2025-01-15 18:43:35 +0200

    Add files via upload

--- project456.py
+++ project456.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jan  9 19:32:42 2025
+Created on Mon Jan 13 11:37:28 2025
 
 @author: geont
 """
+
 import numpy as np
 import pandas as pd
 import warnings
@@ -122,19 +123,61 @@ def fit_arima_model(xV, p, q, d=0, show=False):
         plt.legend(["Residuals", f"Conf. Int {(1 - alpha) * 100}%"])
     return summary, fittedvalues, resid, model, model.aic
 
+def calculate_fitting_error(xV, model, Tmax=20, show=False):
+    '''
+    calculate fitting error with NRMSE for given model in timeseries xV
+    till prediction horizon Tmax
+    returns:
+    nrmseV
+    preds: for timesteps T=1, 2, 3
+    '''
+    nrmseV = np.full(shape=Tmax, fill_value=np.nan)
+    nobs = len(xV)
+    xV_std = np.std(xV)
+    vartar = np.sum((xV - np.mean(xV)) ** 2)
+    predM = []
+    tmin = np.max(
+        [len(model.arparams), len(model.maparams), 1])  # start prediction after getting all lags needed from model
+    for T in np.arange(1, Tmax + 1):
+        errors = []
+        predV = np.full(shape=nobs, fill_value=np.nan)
+        for t in np.arange(tmin, nobs - T):
+            pred_ = model.predict(start=t, end=t + T - 1)
+            # predV.append(pred_[-1])
+            ytrue = xV[t + T - 1]
+            predV[t + T - 1] = pred_[-1]
+            error = pred_[-1] - ytrue
+            errors.append(error)
+        predM.append(predV)
+        errors = np.array(errors)
+        mse = np.mean(np.power(errors, 2))
+        rmse = np.sqrt(mse)
+        nrmseV[T - 1] = (rmse / xV_std)
+        # nrmseV[T] = (np.sum(errors**2) / vartar)
+    if show:
+        fig, ax = plt.subplots(1, 1, figsize=(14, 8))
+        ax.plot(np.arange(1, Tmax + 1), nrmseV[1:], marker='x', label='NRMSE');
+        ax.axhline(1, color='red', linestyle='--');
+        ax.set_title('Fitting Error')
+        ax.legend()
+        ax.set_xlabel('T')
+        ax.set_xticks(np.arange(1, Tmax))
+        plt.show()
+        # #plot multistep prediction for T=1, 2, 3
+        fig, ax = plt.subplots(1, 1, figsize=(14, 8))
+        ax.plot(xV, label='original')
+        colors = ['red', 'green', 'black']
+        for i, preds in enumerate(predM[:3]):
+            ax.plot(preds, color=colors[i], linestyle='--', label=f'T={i + 1}', alpha=0.7)
+        ax.legend(loc='best')
+        plt.show()
+    return nrmseV, predM
+
 team_number = 11
 
 data = pd.read_csv('train.csv') # read csv file
 
 yV = np.array(data.iloc[:, team_number + 1]) / 10**6 # normalized time series
-
-# Plot the original Timeseries
-plt.figure()
-plt.plot(yV)
-plt.xlabel("days")
-plt.ylabel("total daily incoming solar energy (MJ/m^2)")
-plt.title("Original Timeseries")
-plt.show()
 
 dy = np.diff(yV) # first differences of y
 i_zero = [i for i,x in enumerate(dy) if x == 0] # indices where first differences are zero
@@ -153,90 +196,76 @@ for i in i_zero:
         y[i + 1] = 0.5 * (yV[i - 365] + yV[i + 365])
 
 
-### 1. Remove seasonality
+### Remove seasonality
 
 period = 365 # seasonality    
 s = seasonal_components(y, period) # seasonal component
 x = y - s # stationary timeseries
 
-# Plot the stationary timeseries
+### 4. Prediction with Best Linear Model
+Tmax = 30
+len_test = 365
+
+x_train = x[:-len_test] # training set
+x_test = x[-len_test:] # tes set
+
+summary, fittedvalues, resid, model, aic = fit_arima_model(
+    x_train, 2, 3, show = False) # ARMA(2, 3)
+
+x_nrmse, x_pred = calculate_fitting_error(x_test, model, Tmax, show = False)
+x_pred = np.reshape(x_pred, (len(x_pred), len(x_test)))
+
+y_true = np.tile(y[-len_test:], (Tmax, 1)) # why Tmax - 1 ???
+y_pred = x_pred + np.tile(s[-len_test:], (Tmax, 1))
+y_error = y_pred - y_true
+y_mse = np.nanmean(np.power(y_error, 2), axis = 1)
+y_rmse = np.sqrt(y_mse)
+y_nrmse = y_rmse / np.std(y[-len_test:])
+
+'''
+# Plot NRMSE with respect to prediction step T
+fig, ax = plt.subplots(1, 1, figsize=(14, 8))
+ax.plot(np.arange(1, Tmax + 1), y_nrmse, marker='x')
+plt.show()
+'''
+
+### 5. Prediction with Seasonal Trend
+y_train = y[:-len_test] # training set of the non-stationary timeseries
+y_test = y[-len_test:] # tes set of the non-stationary timeseries
+
+
+s_train = seasonal_components(y_train, period)
+m_train = np.mean(y_train)
+
+y_pred_seas = s_train[:len(y_test)] + m_train
+y_error_seas = y_pred_seas - y_true
+y_mse_seas = np.mean(np.power(y_error_seas, 2))
+y_rmse_seas = np.sqrt(y_mse_seas)
+y_nrmse_seas = y_rmse_seas / np.std(y_test)
+
+print(f"Prediction with Best Linear Model: NRMSE = {y_nrmse[0]:.3f} (T=1)")
+print(f"Prediction with Seasonal Trend: NRMSE = {y_nrmse_seas:.3f} (T=1)")
+
 plt.figure()
-plt.plot(x)
+plt.plot(np.arange(len(y) - len(y_test), len(y)), y_test)
+plt.plot(np.arange(len(y) - len(y_test), len(y)), y_pred_seas)
+plt.plot(np.arange(len(y) - len(y_test), len(y)), y_pred[0])
 plt.xlabel("days")
 plt.ylabel("total daily incoming solar energy (MJ/m^2)")
-plt.title("Stationary A Timeseries (without seasonality)")
+plt.title("Predictions on Test Set")
+plt.legend(["Original Data", "Seasonal Model", "Best Linear Model"])
 plt.show()
 
-## 2. White Noise Hypothesis Testing (H0: ρτ = 0)
-max_lag = 10
-alpha = 0.05
+### 6. SNRMSE for Best Linear Model
+y_snrmse = y_nrmse / y_nrmse_seas
 
-# Plot autocorrelation function
-auto_corr = get_acf(x, lags = max_lag)
-
-# Portmanteau test
-ljung_val, ljung_pval = portmanteau_test(x, max_lag)
-print("2. White Noise Hypothesis Testing (H0: ρτ = 0)")
-if ljung_pval < alpha:
-    print(f"p-value = {ljung_pval:.5e}: The stationay A timeseries is not white noise")
-else:
-    print(f"p-value = {ljung_pval:.5e}: The stationay A timeseries is white noise")
-    
-## 3. Search for Best Linear Model
-'''
-# AR(p) Model, Estmation of order p, Fit of AR(p) Model
-partial_auto_corr = get_pacf(x, lags = max_lag)
-
-summary_ar1, fittedvalues_ar1, resid_ar1, model_ar1, aic_ar1 = fit_arima_model(
-    x, 1, 0, show = True) # AR(1)
-
-# MA(q) Model, Estmation of order q, Fit of MA(q) Model
-max_q = 4 # maximum order q tested
-
-for q in np.arange(1, max_q + 1):
-    summary_ma1, fittedvalues_ma1, resid_ma1, model_ma1, aic_ma1 = fit_arima_model(
-        x, 0, q, show = True) # MA(q)
-'''
-# ARMA(p,q) Model
-warnings.filterwarnings("error")
-
-max_p = 4
-max_q = 4
-aic_arma = np.full(shape = (max_p + 1, max_q + 1), fill_value = np.nan)
-ljung_pval = np.full(shape = (max_p + 1, max_q + 1), fill_value = np.nan)
-
-for p in np.arange(max_p + 1):
-    for q in np.arange(max_q + 1):
-        if p != 0 or q != 0:
-            print(f"ARMA({p},{q})---------------------")
-            try:
-                summary, fittedvalues, resid, model, aic_arma[p, q] = fit_arima_model(
-                    x, p, q, show = False) # ARMA(p,q)
-                ljung_val, ljung_pval[p, q] = portmanteau_test(resid, max_lag)
-            except:
-                print("Either non-stationary AR part or non-invertable MA part")
-            
-warnings.resetwarnings()
-
-plt.figure()
-plt.matshow(aic_arma)
-plt.colorbar()
-plt.xticks(np.arange(max_q + 1), [str(i) for i in np.arange(max_q + 1)])
-plt.yticks(np.arange(max_p + 1), [str(i) for i in np.arange(max_p + 1)])
-plt.xlabel("q")
-plt.ylabel("p")
-plt.title("AIC - ARMA(p,q)")
-
-plt.figure()
-plt.matshow(ljung_pval)
-plt.colorbar()
-plt.xticks(np.arange(max_q + 1), [str(i) for i in np.arange(max_q + 1)])
-plt.yticks(np.arange(max_p + 1), [str(i) for i in np.arange(max_p + 1)])
-plt.xlabel("q")
-plt.ylabel("p")
-plt.title("p-value (Portmanteau Test for residuals) - ARMA(p,q)")
-
-'''
-summary, fittedvalues, resid, model, aic_arma = fit_arima_model(
-    x, 2, 3, show = True) # ARMA(2, 3)
-'''
+# Plot NRMSE and SNRMSE with respect to prediction step T
+fig, ax = plt.subplots(1, 1, figsize=(14, 8))
+ax.plot(np.arange(1, Tmax + 1), y_nrmse, marker='x')
+ax.plot(np.arange(1, Tmax + 1), y_snrmse, marker='o')
+ax.axhline(1, color='red', linestyle='--')
+ax.set_title('NRMSE and SNRMSE for Best Linear Model')
+ax.set_xlabel('T')
+ax.set_xticks(np.arange(1, Tmax + 1))
+ax.legend(["NRMSE","SNRMSE"])
+plt.show()
